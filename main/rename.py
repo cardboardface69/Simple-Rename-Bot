53a0defa138b0532ac32f569cc4c4475bab494b4
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-02-11 02:08:55 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -4,16 +4,15 @@ from pyrogram import Client, filters, enums
 from config import temp, CAPTION, ADMIN
 from main.utils import progress_message, humanbytes
 
-@Client.on_message(filters.private & filters.user(ADMIN))             
+@Client.on_message(filters.private & filters.document & filters.user(ADMIN))             
 async def rename_file(bot, msg):
     reply = msg.reply_to_message
-    media = msg.document or msg.audio or msg.video
-    og_media = getattr(reply, reply.media.value)
+    og_media = getattr(msg, msg.media.value)
     new_name = "hell.mkv"
     new_namex = new_name.replace(".mkv", "")
-    sts = await media.reply_text("Trying to Downloading.....")
+    sts = await msg.reply_text("Trying to Downloading.....")
     c_time = time.time()
-    downloaded = media.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
+    downloaded = msg.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
     filesize = humanbytes(og_media.file_size)                
     if CAPTION:
         try:
