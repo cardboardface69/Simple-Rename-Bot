22c9a4819afa5eb95e54dc94c9891f2b7b261ecf
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-02-12 14:18:31 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -27,7 +27,7 @@ async def rename_file(bot, msg):
     filename = filename.replace("720p", "720p x265 BD")
     filename = filename.replace("@Anime_Gallery", "@animxt")
     filename = filename.replace("[Judas]", "")
-    filename = filename.replace(".mkv", " [1080p x265 BD][Dual] @animxt.mkv"
+    filename = filename.replace(".mkv", " [1080p x265 BD][Dual] @animxt.mkv")
     try:
         await bot.send_document(msg.chat.id, document=downloaded, file_name=filename, caption=filename.replace(".mkv", ""), progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
     except Exception as e:  
