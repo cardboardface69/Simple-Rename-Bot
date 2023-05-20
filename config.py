ad8ef967c213b010d6f4cfa788bb7544859fd607
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-05-20 08:28:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,14 @@ CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "")
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "10247139"
+
+API_HASH = "96b46175824223a33737657ab943fd6a"
+
+BOT_TOKEN = "5222572158:AAENHtTOnhWBh4UUZKTjq5ruMtil_4zRA_0"
